9d0cd7f801529b62f4b91572d16ec9e814bfea92
leg883 <leg883@126.com>
2013-01-09 08:40:22 -0800

    Create zhTW.lua

---
+++ localization/zhTW.lua
@@ -0,0 +1,34 @@
+﻿
+local L = tdCore:NewLocale(..., 'zhTW')
+
+if not L then return end
+
+L['Hide blizz cooldown model'] = '隱藏暴雪冷卻模型'
+L['Hide timer when has charges'] = '技能可用時不顯示冷卻計時'
+L['Minimum duration to display text as MM:SS'] = '低於10分鐘的計時以 MM:SS 格式顯示'
+L['Remaining how long after the start timer'] = '剩餘多長時間后開始計時'
+L['Minimum duration to display text'] = '允許計時的最小冷卻時間'
+L['Minimum size to display text'] = '允許計時的最小大小'  --這個不明白...
+L['Style'] = '風格'
+L['Soon'] = '即將完成'
+L['Second'] = '秒'
+L['Minute'] = '分鐘'
+L['Hour'] = '小時'
+L['Font'] = '字體'
+L['Font Size'] = '字體大小'
+L['Font Outline'] = '字體描邊'
+L['Anchor'] = '錨點'
+L['TopLeft'] = '左上'
+L['Top'] = '上'
+L['TopRight'] = '右上'
+L['Left'] = '左'
+L['Center'] = '中'
+L['Right'] = '右'
+L['BottomLeft'] = '左下'
+L['Bottom'] = '下'
+L['BottomRight'] = '右下'
+L['Shine'] = '閃光'
+L['Minimum duration to display shine'] = '允許閃光的最小冷卻時間'
+L['Shine scale'] = '閃光大小'
+L['Shine duration'] = '閃光持續時間'
+L['Shine alpha'] = '閃光透明度'
